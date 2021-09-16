411d562a2cb01529c637c4450376c45cd270ec36
Quantumgames Studios <33845038+jetmaster101@users.noreply.github.com>
2021-09-16 17:25:49 +0000

    update readme.md and main code

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 AI assistant
 QuantumAI is an AI voice assistant to help you with your day. 
 it can tell you the weather, look things up for you, or open websites. 
-
+you can not run this on the web! it results as a ALSA error
 # VERSION
 version dev 1.0.0
 whats new:
@@ -11,3 +11,8 @@ whats new:
 this is made by quantumgames studios.
 # commands
 coming soon
+# running
+before you run it in a console, you have to do 
+pip install speechrecognition
+pip install playsound
+pip install gtts
--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+from gtts import gTTS
 import os
-import time
-import playsound as ps
+import playsound
 import speech_recognition as sr
-from gtts import gTTS 
+import time
 
 def speak(text):
-  tts = gTTS(text=text, lang="en")
-  filename = "voice.mp3"
-  tts.save(filename)
-  ps(filename)
+    tts = gTTS(text=text, lang='en')
+    filename = "voice.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
+def get_audio():
+  r = sr.Recognizer()
+  with sr.Microphone() as source:
+    audio = r.listen(source)
+    said = ""
 
+    try:
+      said = r.recognize_google(audio)
+      print(said)
+    except Exception as e:
+      print('Exception: ' + str(e))
+  return said
+  
 speak("hello boss! what can i help you with?")
-
+get_audio()
